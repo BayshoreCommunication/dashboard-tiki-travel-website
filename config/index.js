acbb9acf059cacebc0152ae2ec35d62a3a05e454
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2025-01-01 14:21:33 +0600

    init

---
+++ config/index.js
@@ -0,0 +1,4 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const CLOUDINARY_CLOUD_NAME = 'dhpzoi6pb';
+export const CLOUDINARY_UPLOAD_PRESET = 'melamedblogs';
+export const CLOUDINARY_FOLDER = 'blog';
