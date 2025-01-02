314a6e4339bcc894888921e5ef76caa7d39e338a
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2025-01-02 14:33:42 +0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const CLOUDINARY_CLOUD_NAME = '';
-export const CLOUDINARY_UPLOAD_PRESET = '';
-export const CLOUDINARY_FOLDER = '';
+export const CLOUDINARY_CLOUD_NAME = 'djwzr5m6g';
+export const CLOUDINARY_UPLOAD_PRESET = 'tikiBlogs';
+export const CLOUDINARY_FOLDER = 'blog';
