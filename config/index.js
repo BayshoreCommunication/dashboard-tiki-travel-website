96c83f9a6f6a76cad10f5849f03ec97fd70cc105
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2025-01-02 11:58:06 +0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const CLOUDINARY_CLOUD_NAME = 'dhpzoi6pb';
-export const CLOUDINARY_UPLOAD_PRESET = 'melamedblogs';
-export const CLOUDINARY_FOLDER = 'blog';
+export const CLOUDINARY_CLOUD_NAME = '';
+export const CLOUDINARY_UPLOAD_PRESET = '';
+export const CLOUDINARY_FOLDER = '';
