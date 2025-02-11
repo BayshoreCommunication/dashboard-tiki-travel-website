f3ec0082ab18ae72314371c360d50a7536a7a4c5
rahad-dev24 <rahad.dev24@gmail.com>
2025-02-11 11:41:26 +0600

    added digital ocean

--- config/index.js
+++ config/index.js
@@ -1,4 +1,15 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const CLOUDINARY_CLOUD_NAME = 'djwzr5m6g';
-export const CLOUDINARY_UPLOAD_PRESET = 'tikiBlogs';
-export const CLOUDINARY_FOLDER = 'blog';
+
+// Old Image Upload Configuration Cloudinary
+
+export const CLOUDINARY_CLOUD_NAME = "djwzr5m6g";
+export const CLOUDINARY_UPLOAD_PRESET = "tikiBlogs";
+export const CLOUDINARY_FOLDER = "blog";
+
+// New Image Upload Configuration Digital Ocean
+
+export const DIGITALOCEAN_ENDPOINT = "https://nyc3.digitaloceanspaces.com";
+export const DIGITALOCEAN_API_KEY = "DO00HFNWVPYJBRTBVZZJ";
+export const DIGITALOCEAN_SECRET_ACCESS_KEY =
+  "VTaTd4LASdw+OjDylmiBOPa4GOD956R/25597C7ULX8";
+export const DIGITALOCEAN_FOLDER = "tiki-travels";
